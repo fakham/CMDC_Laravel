c66189f81b70ababf145d14f2bfb3c7c8e00a33f
fakham <mohammed.fakham@gmail.com>
2018-05-13 12:44:19 +0000

    add client function

---
+++ app/Http/Controllers/ClientController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Client;
+
+class ClientController extends Controller
+{
+    public function add(Request $request) {
+
+        $this->validate(
+            $request,
+            ['nom' => 'required|min:5'],
+            ['text.required' => 'Le nom est obligatoire!'],
+            ['telephone' => 'required|numeric|regex:/(0)([5-7])[0-9]{8}/'],
+            ['telephone.required' => 'Telephone est obligatoire!']
+        );
+
+        $client = new Client;
+        $client->nom = $request->nom;
+        $client->telephone = $request->telephone;
+        $client->activite = $request->activite;
+        $client->region = $request->region;
+        $client->save();
+
+        return redirect(Request::url());
+    }
+}
