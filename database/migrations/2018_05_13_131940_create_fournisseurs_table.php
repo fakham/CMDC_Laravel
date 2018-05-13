91b538cda5fb6af2710d4e59e78a083974aa0ac1
fakham <mohammed.fakham@gmail.com>
2018-05-13 13:20:53 +0000

    fournisseur migration

---
+++ database/migrations/2018_05_13_131940_create_fournisseurs_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateFournisseursTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('fournisseurs', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('nom');
+            $table->string('telephone');
+            $table->string('activite');
+            $table->string('region');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('fournisseurs');
+    }
+}
