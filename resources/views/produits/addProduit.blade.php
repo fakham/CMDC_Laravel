801ee9b7b60c39cd7cad46a79d8b664c2d0bd585
fakham <mohammed.fakham@gmail.com>
2018-05-13 12:28:39 +0000

    ajouter client popup

--- resources/views/produits/addProduit.blade.php
+++ resources/views/produits/addProduit.blade.php
@@ -34,6 +34,51 @@
                         </select>
                     </div>
                     <br>
+                    <div class="input-group">
+                        <select name="client" class="form-control" id="typeProduit">
+                            <option value="" disabled selected>Client..</option>
+                            <option>Client 1</option>
+                            <option>Client 2</option>
+                            <option>Client 3</option>
+                        </select>
+                        <button class="btn btn-default" style="margin-left: 10px" data-toggle="modal" data-target="#clientModal">Ajouter</button>
+
+                        <!-- Modal -->
+                        <div class="modal fade" id="clientModal" role="dialog">
+                            <div class="modal-dialog">
+                            
+                                <!-- Modal content-->
+                                <div class="modal-content">
+                                    <div class="modal-header w-100">
+                                        <h4 class="modal-title w-100">Ajouter Client</h4>
+                                        <button type="button" class="close" data-dismiss="modal">&times;</button>
+                                    </div>
+                                    <div class="modal-body">
+                                        <div class="input-group">
+                                            <input name="nom" class="form-control" type="text" placeholder="Nom Client..."/>
+                                        </div>
+                                        <br>
+                                        <div class="input-group">
+                                            <textarea name="activite" class="form-control" placeholder="Activié..." rows="3"></textarea>
+                                        </div>
+                                        <br>
+                                        <div class="input-group">
+                                            <input name="region" class="form-control" type="text" placeholder="Region..."/>
+                                        </div>
+                                        <br>
+                                        <div class="input-group">
+                                            <input name="telephone" class="form-control" type="text" placeholder="Telephone..."/>
+                                        </div>
+                                    </div>
+                                    <div class="modal-footer">
+                                        <button class="btn btn-default">Ajouter</button>
+                                        <button type="button" class="btn btn-danger" data-dismiss="modal">Annuler</button>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <br>
                     <div class="input-group-btn">
                         <button class="btn btn-default">Enregistrer</button>
                         <a class="btn btn-danger">Annuler</a>
