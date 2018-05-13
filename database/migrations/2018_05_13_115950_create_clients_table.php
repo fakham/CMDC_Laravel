33cfdf867ae88081df625c768756404612adc10a
fakham <mohammed.fakham@gmail.com>
2018-05-13 12:06:23 +0000

    clients model and migration

---
+++ database/migrations/2018_05_13_115950_create_clients_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateClientsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('clients', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('nom');
+            $table->string('telephone');
+            $table->string('activite');
+            $table->string('region');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('clients');
+    }
+}
