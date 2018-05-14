fe804ca2f96b570cf82ba81e572b68e4c6622df6
fakham <mohammed.fakham@gmail.com>
2018-05-14 19:10:51 +0000

    edit page fournisseur

---
+++ resources/views/fournisseurs/editPage.blade.php
@@ -0,0 +1,41 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">Modifier Fournisseur</div>
+
+                    <div class="card-body">
+                        <form method="POST" action="">
+                            @csrf
+                            <div class="input-group">
+                                <input name="nom" class="form-control" type="text" placeholder="Nom Fournisseur..." value='{{ $fournisseur->nom }}'/>
+                            </div>
+                            <br>
+                            <div class="input-group">
+                                <textarea name="activite" class="form-control" placeholder="Activité..." rows="3">{{ $fournisseur->activite }}</textarea>
+                            </div>
+                            <br>
+                            <div class="input-group">
+                                <input name="region" class="form-control" type="text" placeholder="Region..." value='{{ $fournisseur->region }}'/>
+                            </div>
+                            <br>
+                            <div class="input-group">
+                                <input name="telephone" class="form-control" type="text" placeholder="Telephone..." value='{{ $fournisseur->telephone }}'/>
+                            </div>
+                            <br>
+                            <input type="submit" class="btn btn-default" value="Enregistrer"/>
+                            <button type="button" class="btn btn-danger" data-dismiss="modal">Annuler</button>
+                        </form>
+                                    
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+@endsection
