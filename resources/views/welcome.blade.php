4674e2342171b2a8955bc14ebacb70e62764475d
fakham <mohammed.fakham@gmail.com>
2018-05-19 01:45:02 +0000

    welcome page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,87 +1,389 @@
 <!doctype html>
-<html lang="{{ app()->getLocale() }}">
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+<html class="no-js" lang="zxx">
 
-        <title>Laravel</title>
 
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
+<meta http-equiv="content-type" content="text/html;charset=UTF-8" />
 
-        <!-- Styles -->
-        <style>
-            html, body {
-                background-color: #fff;
-                color: #636b6f;
-                font-family: 'Raleway', sans-serif;
-                font-weight: 100;
-                height: 100vh;
-                margin: 0;
-            }
+<head>
 
-            .full-height {
-                height: 100vh;
-            }
+    <title>CMDC APP</title>
+    <!-- Metas -->
+    <meta charset="utf-8">
+    <link rel="icon" type="/public/image/png" href="{{ asset('login/images/icons/favicon.ico')}}" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    
+    <meta name="description" content="Hisham is a responsive creative template">
+    <meta name="keywords" content="portfolio, personal, corporate, business, parallax, creative, agency">
 
-            .flex-center {
-                align-items: center;
-                display: flex;
-                justify-content: center;
-            }
+    <!-- bootstrap css -->
+    <link rel="stylesheet" href="{{ asset('css/bootstrap.min.css') }}">
 
-            .position-ref {
-                position: relative;
-            }
+    <!-- google fonts -->
+    <link href="https://fonts.googleapis.com/css?family=Muli:200,300,400,600,700,,800,900" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Josefin+Sans:300,400,600,700" rel="stylesheet">
 
-            .top-right {
-                position: absolute;
-                right: 10px;
-                top: 18px;
-            }
+    <!-- owl carousel CSS -->
+    <link rel="stylesheet" href="{{ asset('css/owl.carousel.min.css')}}">
+    <link rel="stylesheet" href="{{ asset('css/owl.theme.default.min.css')}}">
 
-            .content {
-                text-align: center;
-            }
+    <!-- magnific-popup CSS -->
+    <link rel="stylesheet" href="{{ asset('css/magnific-popup.css')}}">
+    <link rel="stylesheet" href="{{ asset('css/singing.css')}}">
 
-            .title {
-                font-size: 84px;
-            }
+    <!-- Font Icon Core CSS -->
+    <link rel="stylesheet" href="{{ asset('css/font-awesome.min.css')}}">
+    <link rel="stylesheet" href="{{ asset('css/et-line.css')}}">
 
-            .links > a {
-                color: #636b6f;
-                padding: 0 25px;
-                font-size: 12px;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
-            }
+    <!-- Core Style Css -->
+    <link rel="stylesheet" href="{{ asset('css/style.css')}}">
 
-            .m-b-md {
-                margin-bottom: 30px;
-            }
-        </style>
-    </head>
-    <body>
-        <div class="flex-center position-ref full-height">
-            @if (Route::has('login'))
-                <div class="top-right links">
-                    @auth
-                        <a href="{{ url('/home') }}">Home</a>
-                    @else
-                        <a href="{{ route('login') }}">Login</a>
-                        <a href="{{ route('register') }}">Register</a>
-                    @endauth
-                </div>
-            @endif
 
-            <div class="content">
-                <div class="title m-b-md">
-                    CMDC
-                </div>
+</head>
+
+<body>
+
+    <div id="main">
+
+        <!-- ====== Preloader ======  -->
+        <div class="loading">
+            <div class="load-circle">
             </div>
         </div>
-    </body>
+        <!-- ======End Preloader ======  -->
+
+
+        <!-- ====== button-top ======  -->
+        <div class="button-top" data-scroll-nav="0">
+            <span>
+				<i class="fa fa-info-" aria-hidden="true"></i>
+			</span>
+        </div>
+        <!-- ======End button-top ======  -->
+
+        <!-- ====== Navgition ======  -->
+        <nav class="navbar navbar-default">
+            <div class="container">
+                <!-- Brand and toggle get grouped for better mobile display -->
+                <div class="navbar-header">
+                    <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#nav-icon-collapse" aria-expanded="false">
+		        <span class="sr-only">Toggle navigation</span>
+		        <span class="icon-bar"></span>
+		        <span class="icon-bar"></span>
+		        <span class="icon-bar"></span>
+		      </button>
+
+                    <!-- logo -->
+                    <a href="#">
+                        <div class="logo">CMDC App</div>
+                    </a>
+
+
+                </div>
+
+                <!-- Collect the nav links, and other content for toggling -->
+
+                <!-- /.navbar-collapse -->
+                <div class="collapse navbar-collapse" id="nav-icon-collapse">
+
+                    <!-- links -->
+                    <ul class="nav navbar-nav navbar-right">
+                        <li><a href="#" data-scroll-nav="0" class="">Home</a></li>
+                        <li><a href="#" data-scroll-nav="1" class="">About</a></li>
+                        <li><a href="#" data-scroll-nav="2" class="">Portfolio</a></li>
+                        <li><a href="#" data-scroll-nav="3" class="">Testimonails</a></li>
+                        <li><a href="#" data-scroll-nav="4" class="">Blog</a></li>
+                        <li><a href="#" data-scroll-nav="5">Contact</a></li>
+                        <li><a href="login.html"style="font-size: 48px "><font color="#d63561">Login</font></a></li>
+                    </ul>
+                </div>
+            </div>
+            <!-- /.container -->
+        </nav>
+
+
+
+
+        <!-- ====== Header ======  -->
+        <section id="home" class="header bg-img" data-scroll-index="0" data-overlay-dark="6" data-background="{{ asset('img/bg.jpg')}}" data-stellar-background-ratio="0.5">
+            <div class="container">
+                <div class="row">
+                    <div class="v-middle">
+                        <div class="caption">
+                            <h4>CMDC App</h4>
+                            <h1>La comtabilité simplifié!</h1>
+                            <h6 class="cd-headline clip">
+                                <span class="cd-words-wrapper">
+					              <b class="is-visible">Initialise</b>
+					              <b>Réalise</b>
+					              <b>Analyse</b>
+                                
+					            </span>
+                            </h6>
+                           
+                               <div class="btn">
+                            <a href="signUp.html"><button type="submit" class="buton" >Commencer</button></a> 
+                            
+                            </div>
+                            
+                            
+
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <!-- ====== End Header ======  -->
+
+        <!-- ====== Hero ======  -->
+        <section id="about" class="hero section-padding" data-scroll-index="1">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-4">
+                        <div class="hero-img">
+                            <img src="{{ asset('/img/hero.jpg')}}" alt="">
+                        </div>
+                    </div>
+                    <div class="col-md-offset-1 col-md-7">
+                        <div class="content">
+                            <h3>CMDC App Solution</h3>
+                            <h6>Comptabilité simplifié pour artisan et entrepreneurs!</h6>
+                            <p>L’analyse des comptes de ses fournisseurs peut être une mine d’informations et procure des avantages indéniables. C’est une aide incontestable si l’on veut sécuriser ses approvisionnements, mieux négocier ses contrats et réaliser des économies. Vous avez toujours pensé que ce serait bien de fqire votre comptabilité, mais que cela risquait d’être fastidieux. Il convient de relativiser. Voici une solution dont le but est de faciliter votre comptabilité, pour finalement la considérer facile. Si vous désirez mieux suivre l’évolution de votre entreprise afin d’agir et réagir sur certains chiffres, d’éclairer votre vision sur sa situation financière, d’envisager des investissements pour la développer, il est vraiment indispensable que vous acquériez des infos comptables et appréhendiez ses comptes avec objectivité.
+                            </p>
+                            <div class="row">
+                                <div class="col-sm-6">
+                                    <ul>
+                                        <li><span>Date de lancement : </span>Mai 2018</li>
+                                        <li><span>Phone : </span>+212 6 61 00 00 00</li>
+                                    </ul>
+                                </div>
+                                <div class="col-sm-6">
+                                    <ul>
+                                        <li><span>Email : </span>cmdc@gmail.com</li>
+                                        <li><span>Address : </span>Berrechid </li>
+                                    </ul>
+                                </div>
+                                <div class="clear-fix"></div>
+                            </div>
+
+
+                        </div>
+                    </div>
+                    <div class="clear-fix"></div>
+
+
+                </div>
+            </div>
+        </section>
+        <!-- ====== End Hero ======  -->
+
+        <!-- ======sing up ======  -->
+
+
+        <!--====== Services ======-->
+        <section class="services">
+            <div class="container">
+                <div class="row">
+                    <div class="owl-carousel owl-theme">
+                        <div class="sitem">
+                            <span class="icon icon-laptop"></span>
+                            <div class="content">
+                                <h6>Initialisation</h6>
+                                <p>Chaque tenant de comptabilité peux initialiser le programme selon ses propres constatations de charges ou de recette , spécifique à son activité.</p>
+                            </div>
+                        </div>
+                        <div class="sitem">
+                            <span class="icon icon-megaphone"></span>
+                            <div class="content">
+                                <h6>Manage</h6>
+                                <p>Chaque tenant de comptabiliter peux ajouter , éditer, suprimer des charges ou des recettes précédemment initialisé .</p>
+                            </div>
+                        </div>
+                        <div class="sitem">
+                            <span class="icon icon-layers"></span>
+                            <div class="content">
+                                <h6>Development</h6>
+                                <p>Lorem Ipsum is simply dummy text of the printing and type setting industry took a galley of type and scrambled.</p>
+                            </div>
+                        </div>
+                        <div class="sitem">
+                            <span class="icon icon-aperture"></span>
+                            <div class="content">
+                                <h6>Marketing</h6>
+                                <p>Lorem Ipsum is simply dummy text of the printing and type setting industry took a galley of type and scrambled.</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <!--====== End Services ======-->
+
+        <!--====== Portfolio ======-->
+
+        <!--====== End Portfolio ======-->
+
+
+        <!--====== End Numbers ======-->
+
+        <!--====== Clients ======-->
+        <section id="clients" class="clients section-padding" data-scroll-index="3">
+            <div class="container">
+                <div class="row">
+                    <div class="col-sm-10 col-sm-offset-1 col-md-8 col-md-offset-2">
+
+                        <div class="text-center mb-30">
+                            <span class="icon icon-quote"></span>
+                        </div>
+
+                        <div class="owl-carousel owl-theme text-center">
+                            <div class="citem">
+                                <h6>Alex Smith</h6>
+                                <p>Lorem ipsum dolor sit amet, conse ctetur elit,sed do eiusmod tempor incid idunt ut labore et dolore magna aliqua.Ut enim ad minim veniam,quis nostrud exerci ipsum dolor sit amet.</p>
+                                <span>......</span>
+                            </div>
+                            <div class="citem">
+                                <h6>Alex Smith</h6>
+                                <p>Lorem ipsum dolor sit amet, conse ctetur elit,sed do eiusmod tempor incid idunt ut labore et dolore magna aliqua.Ut enim ad minim veniam,quis nostrud exerci ipsum dolor sit amet.</p>
+                                <span>.......</span>
+                            </div>
+                            <div class="citem">
+                                <h6>Alex Smith</h6>
+                                <p>Lorem ipsum dolor sit amet, conse ctetur elit,sed do eiusmod tempor incid idunt ut labore et dolore magna aliqua.Ut enim ad minim veniam,quis nostrud exerci ipsum dolor sit amet.</p>
+                                <span>........</span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <!--====== End Clients ======-->
+
+
+        <!--====== Contact ======-->
+        <section id="contact" class="contact section-padding pb-0" data-scroll-index="5">
+            <div class="container">
+
+                <div class="section-head">
+                    <h4>Get In Touch</h4>
+                </div>
+
+                <form class='form' id='contact-form' method='post'><input type='hidden' name='form-name' value='contact-form' onsubmit='return false'/>
+                    <div class="messages"></div>
+
+                    <div class="controls">
+
+                        <div class="row">
+                            <div class="col-md-6">
+                                <div class="form-group">
+                                    <input id="form_name" type="text" name="name" placeholder="Name *" required="required">
+                                </div>
+                            </div>
+
+                            <div class="col-md-6">
+                                <div class="form-group">
+                                    <input id="form_email" type="email" name="email" placeholder="Email *" required="required">
+                                </div>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col-md-12">
+                                <div class="form-group">
+                                    <textarea id="form_message" name="message" placeholder="Your Message *" rows="4" required="required"></textarea>
+                                </div>
+                            </div>
+                            <div class="col-md-12">
+                                <div class="submit">
+                                    <span><i class="fa fa-info-circle" aria-hidden="true"></i>All the fields are required</span>
+                                    <input type="submit" value="Submit">
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </form>
+            </div>
+
+            <div class="copy-right">
+                <div class="social-icon">
+                    <div class="container text-center">
+                        <div class="row">
+                            <a href="#0">
+								<span><i class="fa fa-facebook" aria-hidden="true"></i></span>
+							</a>
+                            <a href="#0">
+								<span><i class="fa fa-twitter" aria-hidden="true"></i></span>
+							</a>
+                            <a href="#0">
+								<span><i class="fa fa-linkedin" aria-hidden="true"></i></span>
+							</a>
+                            <a href="#0">
+								<span><i class="fa fa-behance" aria-hidden="true"></i></span>
+							</a>
+                            <a href="#0">
+								<span><i class="fa fa-instagram" aria-hidden="true"></i></span>
+							</a>
+                        </div>
+                    </div>
+                </div>
+                <p>&copy; Made with love for great people like Youssef, Ismail & Mohammed.</p>
+            </div>
+        </section>
+        <!--====== sidebare ======-->
+    </div>
+
+
+
+    <!-- jQuery -->
+    <script src="{{ asset('js/jquery-3.0.0.min.js')}}"></script>
+    <script src="{{ asset('js/jquery-migrate-3.0.0.min.js')}}"></script>
+
+    <!-- bootstrap -->
+    <script src="{{ asset('js/bootstrap.min.js')}}"></script>
+
+    <!-- scrollIt -->
+    <script src="{{ asset('js/scrollIt.min.js')}}"></script>
+
+    <!-- magnific-popup -->
+    <script src="{{ asset('js/jquery.magnific-popup.min.js')}}"></script>
+
+    <!-- owl carousel -->
+    <script src="{{ asset('js/owl.carousel.min.js')}}"></script>
+
+    <!-- stellar js -->
+    <script src="{{ asset('js/jquery.stellar.min.js')}}"></script>
+
+    <!-- animated.headline js -->
+    <script src="{{ asset('js/animated.headline.js')}}"></script>
+
+    <!-- jquery.waypoints.min js -->
+    <script src="{{ asset('js/jquery.waypoints.min.js')}}"></script>
+
+    <!-- jquery.counterup.min js -->
+    <script src="{{ asset('js/jquery.counterup.min.js')}}"></script>
+
+    <!-- isotope.pkgd.min js -->
+    <script src="{{ asset('js/isotope.pkgd.min.js')}}"></script>
+
+    <!-- validator js -->
+    <script src="{{ asset('js/validator.js')}}"></script>
+
+    <!-- custom script -->
+    <script src="{{ asset('js/custom.js')}}"></script>
+    <!--- functionload
+    <script>
+        // Get the modal
+        var modal = document.getElementById('id01');
+
+        // When the user clicks anywhere outside of the modal, close it
+        *window.onclick = function(event) {
+            if (event.target == modal) {
+                modal.style.display = "none";
+            }
+        }
+   
+
+    </script>
+    -->
+</body>
+
 </html>
