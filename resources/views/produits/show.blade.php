d9f7b82ccb3032047c9ef71aa9f081ecf7dbbba1
fakham <mohammed.fakham@gmail.com>
2018-05-26 13:20:32 +0000

    new table produits

--- resources/views/produits/show.blade.php
+++ resources/views/produits/show.blade.php
@@ -1,92 +1,89 @@
 @extends('../layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
+<div class="container-fluid">
+    <div class="row">
+        <div class="col-md-12">
             <div class="card">
-                <div class="card-header">Programmer</div>
+                <div class="header">
+                    <h4 class="title">Table Produits</h4>
+                    <p class="category"></p>
+                </div>
+                <div class="content table-responsive table-full-width">
+                    <table class="table table-striped">
+                        <thead>
+                            <th>Nom</th>
+                            <th>Description</th>
 
-                <div class="card-body">
-                    <div class="col-xs-6">
-                        <h2 class="sub-header"><a data-toggle="collapse" href="#produits">Table Produits</a></h2>
-                        <div id="produits" class="panel-collapse collapse">
-                            <div class="table-responsive">  
-                                <table class="table table-striped" width="100"style="float:left;">
-                                    <thead>
-                                    <tr>
-                                        <th>Nom</th>
-                                        <th>Description</th>
-                                    </tr>
-                                    </thead>
-                                    <tbody>
-                                        @foreach ($produits as $produit)
-                                        <tr>
-                                            <td>{{ $produit->nom }}</td>
-                                            <td>{{ $produit->description }}</td>
-                                            <td><a type="button" class="btn btn-default" href="produit/{{ $produit->id }}/edit">Modifier</a></td>
-                                            <td><a class="btn btn-danger" onclick="return confirm('Voulez-vous supprimer le produit (\'{{ $produit->nom }}\')?')" href="produit/{{ $produit->id }}/delete">Supprimer</a></td>
-                                        </tr>
-                                        @endforeach
-                                    </tbody>
-                                </table>
-                            </div>
-                        </div>
+
+                        </thead>
+                        <tbody>
+                            @foreach ($produits as $produit)
+                            <tr>
+                                <td>{{ $produit->nom }}</td>
+                                <td>{{ $produit->description }}</td>
+                                <td><a type="button" class="btn btn-default" href="produit/{{ $produit->id }}/edit">Modifier</a></td>
+                                <td><a class="btn btn-danger" onclick="return confirm('Voulez-vous supprimer le produit (\'{{ $produit->nom }}\')?')" href="produit/{{ $produit->id }}/delete">Supprimer</a></td>
+                            </tr>
+                            @endforeach
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+
+        <div class="col-md-12">
+                <div class="card">
+                    <div class="header">
+                        <h4 class="title">Table Fournisseurs</h4>
+                        <p class="category"></p>
                     </div>
+                    <div class="content table-responsive table-full-width">
+                        <table class="table table-striped">
+                            <thead>
+                                <th>Nom</th>
+                                <th>Telephone</th>
 
-                    <div class="col-xs-6">
-                        <h2 class="sub-header"><a data-toggle="collapse" href="#fournisseurs">Table Fournisseurs</a></h2>
-                        <div id="fournisseurs" class="panel-collapse collapse">
-                            <div class="table-responsive">  
-                                <table class="table table-striped" width="100"style="float:left;">
-                                    <thead>
-                                    <tr>
-                                        <th>Nom</th>
-                                        <th>Telephone</th>
-                                    </tr>
-                                    </thead>
-                                    <tbody>
-                                        @foreach ($fournisseurs as $fournisseur)
-                                        <tr>
-                                            <td>{{ $fournisseur->nom }}</td>
-                                            <td>{{ $fournisseur->telephone }}</td>
-                                            <td><a type="button" class="btn btn-default" href="fournisseur/{{ $fournisseur->id }}/edit">Modifier</a></td>
-                                            <td><a class="btn btn-danger" onclick="return confirm('Voulez-vous supprimer le fournisseur (\'{{ $fournisseur->nom }}\')?')" href="fournisseur/{{ $fournisseur->id }}/delete">Supprimer</a></td>
-                                        </tr>
-                                        @endforeach
-                                    </tbody>
-                                </table>
-                            </div>
-                        </div>
+                            </thead>
+                            <tbody>
+                                @foreach ($fournisseurs as $fournisseur)
+                                <tr>
+                                    <td>{{ $fournisseur->nom }}</td>
+                                    <td>{{ $fournisseur->telephone }}</td>
+                                    <td><a type="button" class="btn btn-default" href="fournisseur/{{ $fournisseur->id }}/edit">Modifier</a></td>
+                                    <td><a class="btn btn-danger" onclick="return confirm('Voulez-vous supprimer le fournisseur (\'{{ $fournisseur->nom }}\')?')" href="fournisseur/{{ $fournisseur->id }}/delete">Supprimer</a></td>
+                                </tr>
+                                @endforeach
+                            </tbody>
+                        </table>
                     </div>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-12">
+            <div class="card">
+                <div class="header">
+                    <h4 class="title">Table Clients</h4>
+                    <p class="category"></p>
+                </div>
+                <div class="content table-responsive table-full-width">
+                    <table class="table table-striped">
+                        <thead>
+                            <th>Nom</th>
+                            <th>Telephone</th>
 
-                    <div class="col-xs-6">
-                        <h2 class="sub-header"><a data-toggle="collapse" href="#clients">Table Clients</a></h2>
-                        <div id="clients" class="panel-collapse collapse">
-                            <div class="table-responsive">  
-                                <table class="table table-striped" width="100"style="float:left;">
-                                    <thead>
-                                    <tr>
-                                        <th>Nom</th>
-                                        <th>Telephone</th>
-                                    </tr>
-                                    </thead>
-                                    <tbody>
-                                        @foreach ($clients as $client)
-                                        <tr>
-                                            <td>{{$client->nom}}</td>
-                                            <td>{{$client->telephone}}</td>
-                                            <td><a type="button" class="btn btn-default" href="client/{{ $client->id }}/edit">Modifier</a></td>
-                                            <td><a class="btn btn-danger" onclick="return confirm('Voulez-vous supprimer le client (\'{{ $client->nom }}\')?')" href="client/{{ $client->id }}/delete">Supprimer</a></td>
-                                        </tr>
-                                        @endforeach
-                                    </tbody>
-                                </table>
-                            </div>
-                        </div>
-                    </div>
-
-
+                        </thead>
+                        <tbody>
+                            @foreach ($clients as $client)
+                            <tr>
+                                <td>{{$client->nom}}</td>
+                                <td>{{$client->telephone}}</td>
+                                <td><a type="button" class="btn btn-default" href="client/{{ $client->id }}/edit">Modifier</a></td>
+                                <td><a class="btn btn-danger" onclick="return confirm('Voulez-vous supprimer le client (\'{{ $client->nom }}\')?')" href="client/{{ $client->id }}/delete">Supprimer</a></td>
+                            </tr>
+                            @endforeach
+                        </tbody>
+                    </table>
                 </div>
             </div>
         </div>
